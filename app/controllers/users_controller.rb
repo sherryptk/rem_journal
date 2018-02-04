61a4021900a37ab5106e5c7323c76fa4570fd0be
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-02-04 08:23:25 -0500

    added signup and helper methods to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect to '/tweets'
+    end
+    erb :'/users/create_user'
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+  
 end
