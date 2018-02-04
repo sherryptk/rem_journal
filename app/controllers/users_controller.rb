8f9edb6207927cdd5871901beca986e46de757ff
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-02-04 09:00:24 -0500

    setup login route and added form to view, setup logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,29 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+  if logged_in?
+    redirect to '/dreams'
+  else
+    erb :'/users/login'
+  end
+end
+
+  post '/login' do
+    @user = User.find_by(username: params["username"])
+    if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect to '/dreams'
+    else
+        redirect "/login"
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+
   helpers do
     def logged_in?
       !!session[:user_id]
