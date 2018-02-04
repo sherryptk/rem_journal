fbf0b79312e83fd478ac09805304fdfe7c8d4e39
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-02-04 17:00:48 -0500

    added login failed and login success messages using rack flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,14 +35,17 @@ end
     @user = User.find_by(username: params["username"])
     if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
+        flash[:message] = "You have successfully logged in!"
         redirect to '/dreams'
     else
+      flash[:message] = "Sorry, invalid login. Please try again."
         redirect "/login"
     end
   end
 
   get '/logout' do
     session.clear
+    flash[:message] = "You have successfully logged out!"
     redirect '/login'
   end
 
