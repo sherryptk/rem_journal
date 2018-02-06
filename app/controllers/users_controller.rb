b2131425e636149bbd3e1457e9f540330dc7047c
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-02-06 17:42:16 -0500

    added data validation, users must choose an original username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,13 @@ class UsersController < ApplicationController
 
   post '/signup' do
     if params[:username] == '' || params[:email] == '' || params[:password] == ''
-    redirect to '/signup'
+      flash[:message] = "Be sure not to leave any fields blank before submitting!"
+      redirect to '/signup'
+    elsif User.find_by(username: params[:username])
+      flash[:message] = "That username is already taken. Please choose another."
+      redirect to '/signup'
     else
-      @user = User.create(username: params["username"], email: params["email"], password: params["password"])
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
         session[:user_id] = @user.id
         flash[:message] = "You have successfully signed up!"
         redirect to '/dreams'
@@ -32,7 +36,7 @@ class UsersController < ApplicationController
 end
 
   post '/login' do
-    @user = User.find_by(username: params["username"])
+    @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
         flash[:message] = "You have successfully logged in!"
